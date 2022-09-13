b2eb1fce9f47e6222c92e7d83753dbfc600d13e6
Camo282 <camo282@gmail.com>
2022-09-13 10:28:54 -0400

    setting up html

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+//Add questions and link to script.js//
+var questions = [ 
+    {
+    message: "If and Else statements are also referred to as:",
+    choices: [ 
+        "Control flow statements", 
+        "Function statements", 
+        "Variables", 
+        "Operators"
+    ],
+    correctAnswer: "Control flow statements"
+    },
+    {
+        message: "Which of the following is not a way to declare a JavaScript Variable?",
+        choices: [
+            "var", 
+            "let", 
+            "const", 
+            "dis"
+        ],
+        correctAnswer: "dis"
+    },
+    {
+        message: "What html tag is NOT included in the HEAD tag?",
+        choices: ["link",
+         "meta", 
+         "title", 
+         "header"
+        ],
+        correctAnswer: "header"
+    },
+    {
+        message: "JavaScript is which type of language?",
+        choices: [
+            "Object oriented",
+            "Object based",
+            "Assembly language",
+            "High level"
+        ],
+        correctAnswer: "Object based"
+    },
+    ];
